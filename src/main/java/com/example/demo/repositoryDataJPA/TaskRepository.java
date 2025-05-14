8f943130ee8551b6963ad05470e026aa9ee34db3
royal <royalhellt@gmail.com>
2025-05-14 14:53:49 +0500

    Создан интерфейс TaskRepository - интерфейс для работы с БД с помощью Sprint Data JPA

---
+++ src/main/java/com/example/demo/repositoryDataJPA/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repositoryDataJPA;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.entityDB.TaskEntity;
+import com.example.demo.entityDB.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+    @Query("SELECT task FROM TaskEntity task WHERE task.status = 'CONSIDERED' AND task.scheduledTime <= CURRENT TIMESTAMP")
+    List<TaskEntity> findReadyTasks();
+}
