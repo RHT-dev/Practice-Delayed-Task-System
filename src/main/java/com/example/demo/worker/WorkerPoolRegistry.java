508cb65aad43da74a0e1d6ffaa146dd4c5ebc1fe
royal <royalhellt@gmail.com>
2025-05-15 13:52:51 +0500

    Прописана 'первичная' логика класса WorkerPoolRegistry

--- src/main/java/com/example/demo/worker/WorkerPoolRegistry.java
+++ src/main/java/com/example/demo/worker/WorkerPoolRegistry.java
@@ -1,6 +1,40 @@
 package com.example.demo.worker;
 
-// временная заглушка
+import org.springframework.stereotype.Component;
+import com.example.demo.entityDB.TaskEntity;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Component
 public class WorkerPoolRegistry {
+
+    private final Map<String, ExecutorService> pools = new ConcurrentHashMap<>();
+
+    public WorkerPoolRegistry() {
+        pools.put("default", Executors.newFixedThreadPool(4));
+    }
+
+    public static class WorkerPool {
+        private final ExecutorService executor;
+
+        public WorkerPool(ExecutorService executor) {
+            this.executor = executor;
+        }
+
+        public void submit(TaskEntity task) {
+            /*executor.submit(() ->  {
+                System.out.println("TEST: Running this " + task.getId() + " Class: " + task.getTaskClassName());
+                // to do: будущая логика воркера
+
+            })*/
+        }
+    }
+
+    public WorkerPool get(String category) {
+        return new WorkerPool(pools.getOrDefault(category, pools.get("default")));
+    }
 
 }
