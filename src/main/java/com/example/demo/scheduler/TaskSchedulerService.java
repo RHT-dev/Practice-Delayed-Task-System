15075b647288a7a60ece671c6549893a6088e364
royal <royalhellt@gmail.com>
2025-05-16 14:37:42 +0500

    Обновлен класс TaskSchedulerService. Добавлен метод handleFailure() - хендлер при неудачной ситуации: при превышении максимального количества попыток. Добавлен метод calculateNextRuntime() для расчёта времени следующей попытки повтора.

--- src/main/java/com/example/demo/scheduler/TaskSchedulerService.java
+++ src/main/java/com/example/demo/scheduler/TaskSchedulerService.java
@@ -6,6 +6,9 @@ import com.example.demo.repositoryDataJPA.TaskRepository;
 import com.example.demo.worker.WorkerPool;
 import com.example.demo.worker.WorkerPoolRegistry;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +17,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TaskSchedulerService {
@@ -62,5 +67,38 @@ public class TaskSchedulerService {
                 taskRepository.save(task);
             }
         }
+    }
+
+    public void handleFailure(TaskEntity task) {
+        int currentAttempts = task.getAttemptCount();
+        int maxAttempts = task.getMaxAttempts();
+
+        if(currentAttempts >= maxAttempts) {
+            log.warn("Task id:{}, попытки превысили максимальное количество. Статус задачи теперь FAILED", task.getId());
+            task.setStatus(TaskStatus.FAILED);
+        }
+        else {
+            task.setAttemptCount(currentAttempts + 1);
+            task.setStatus(TaskStatus.CONSIDERED);
+            task.setScheduledTime(calculateNextRuntime(task));
+            taskRepository.save(task);
+        }
+    }
+
+    public LocalDateTime calculateNextRuntime(TaskEntity task) {
+        Map<String, Object> retryParams;
+        try {
+            retryParams = objectMapper.readValue(task.getRetryParamsJSON(), new TypeReference<Map<String, Object>>() {});
+        } catch (Exception e) {
+            log.error("FAILED TO PARSE JSON PARAMS. USING 30 SEC DELAY INSTEAD. " + e.getMessage());
+            return LocalDateTime.now().plusSeconds(15); // 15
+        }
+
+        long baseDelay = ((Number) retryParams.getOrDefault("delay", 30)).longValue(); // в секундах
+
+        return switch (task.getRetryType()) {
+            case CONSTANT -> LocalDateTime.now().plusSeconds(baseDelay);
+            case EXPONENT -> LocalDateTime.now().plusSeconds((long) Math.pow(baseDelay, task.getAttemptCount()));
+        };
     }
 }
